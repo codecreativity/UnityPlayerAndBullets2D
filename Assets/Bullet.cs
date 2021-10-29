77d7384b6ce1944103625570981e055d99d75b52
Sebastian Resch <sebastian.resch@student.tugraz.at>
2021-10-29 16:57:02 +0200

    Initial commit

---
+++ Assets/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField]
+    float _speed = 1f;
+
+    [SerializeField]
+    Rigidbody2D _rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this._rigidbody.velocity = transform.right * this._speed;
+    }
+
+    void Update()
+    {
+        if(transform.position.x > 10 || transform.position.x < -10 || transform.position.y > 10 || transform.position.y < -10)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Bullet exited the Player");
+        this.GetComponent<CircleCollider2D>().isTrigger = false;
+    }
+}
---
+++ Assets/BulletSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private float _bulletSpawnIntervalSeconds = 1;
+
+    [SerializeField]
+    private GameObject _bulletPrefab = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnBullet", 2.0f, this._bulletSpawnIntervalSeconds);
+    }
+
+    void SpawnBullet()
+    {
+        Instantiate(this._bulletPrefab, this.transform.position, this.transform.rotation);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z + 5);
+    }
+}
